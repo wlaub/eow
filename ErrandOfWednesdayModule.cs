6906d4823cbb93428cf4a2a68ef5a13bca037599
wlaub <goldcray@gmail.com>
2023-06-26 13:59:41 -0600

    move unique glider functionality into awake function instead of using a hook

--- ErrandOfWednesdayModule.cs
+++ ErrandOfWednesdayModule.cs
@@ -25,68 +25,9 @@ namespace Celeste.Mod.ErrandOfWednesday {
         }
 
         public override void Load() {
-            On.Celeste.Level.LoadLevel += uniqueGliderHook;
         }
 
         public override void Unload() {
-            On.Celeste.Level.LoadLevel -= uniqueGliderHook;
-        }
-
-        private void uniqueGliderHook(On.Celeste.Level.orig_LoadLevel orig, 
-                                        Level level, Player.IntroTypes playerIntro, 
-                                        bool isFromLoader)
-        {
-            orig(level, playerIntro, isFromLoader);
-            Player player = level.Tracker.GetEntity<Player>();
-            UniqueGlider nearest = null;
-            Glider held = null;
-            float best_dist = -1;
-            if (player != null)
-            {
-                //Find the nearest non-held unique jellyfish
-                foreach(UniqueGlider e in level.Tracker.GetEntities<UniqueGlider>())
-                {
-                    if(player.Holding != null && player.Holding == e.Hold)
-                    {
-                        held = e;
-                    }
-                    else if(level.IsInBounds(e))
-                    {
-                    
-                        float dist = Vector2.DistanceSquared(player.Position, e.Position);
-                        if(dist < best_dist || best_dist < 0)
-                        {
-                            best_dist = dist;
-                            nearest = e;
-                        }
-                    }
-                }
-
-                //determine which one to keep
-                if(nearest != null && held != null)
-                {
-                    if(nearest.confiscate)
-                    {
-                        player.Drop();
-                        level.Remove(held);
-                        held = null;
-                    }
-                    else
-                    {
-                        nearest = null;
-                    }
-                }
-
-                //remove all the rest
-                foreach(UniqueGlider e in level.Tracker.GetEntities<UniqueGlider>())
-                {
-                    if(e != nearest && e != held)
-                    {
-                        level.Remove(e);
-                    }
-                } 
-               
-            }
         }
 
     }
--- entities/unique_glider.cs
+++ entities/unique_glider.cs
@@ -16,9 +16,77 @@ namespace Celeste.Mod.ErrandOfWednesday
 
         public bool confiscate;
 
+        public bool awakened = false;
+
         public UniqueGlider(EntityData e, Vector2 offset) : base(e, offset)
         {
             confiscate = e.Bool("confiscate");
+        }
+
+        public override void Awake(Scene scene)
+        {
+            base.Awake(scene);
+
+            if(awakened) return;
+
+            //TODO: currently only confiscates other unique gliders
+            //Might be neat to confiscate any glider for more consistent
+            //behavior, but i don't currently plan to use this with gliders
+            //that aren't unique.
+            if(scene is Level level)
+            {
+                Player player = level.Tracker.GetEntity<Player>();
+                UniqueGlider nearest = null;
+                Glider held = null;
+                float best_dist = -1;
+                if (player != null)
+                {
+                    //Find the nearest non-held unique jellyfish
+                    foreach(UniqueGlider e in level.Tracker.GetEntities<UniqueGlider>())
+                    {
+                        if(player.Holding != null && player.Holding == e.Hold)
+                        {
+                            held = e;
+                        }
+                        else if(level.IsInBounds(e))
+                        {
+                        
+                            float dist = Vector2.DistanceSquared(player.Position, e.Position);
+                            if(dist < best_dist || best_dist < 0)
+                            {
+                                best_dist = dist;
+                                nearest = e;
+                            }
+                        }
+                    }
+
+                    //determine which one to keep
+                    if(nearest != null && held != null)
+                    {
+                        if(nearest.confiscate)
+                        {
+                            player.Drop();
+                            level.Remove(held);
+                            held = null;
+                        }
+                        else
+                        {
+                            nearest = null;
+                        }
+                    }
+
+                    //remove all the rest
+                    foreach(UniqueGlider e in level.Tracker.GetEntities<UniqueGlider>())
+                    {
+                        e.awakened = true;
+                        if(e != nearest && e != held)
+                        {
+                            level.Remove(e);
+                        }
+                    } 
+                   
+                }
+            }       
         }
 
 
