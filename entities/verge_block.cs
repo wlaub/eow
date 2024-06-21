64572c94aa18cfbe4d4370c12044093bdeffa59d
wlaub <goldcray@gmail.com>
2024-06-20 18:38:12 -0600

    this is where i was trying out some kind of gridded wobble outline but decided it was not worth

--- entities/verge_block.cs
+++ entities/verge_block.cs
@@ -18,27 +18,6 @@ namespace Celeste.Mod.ErrandOfWednesday
     {
         public static bool loaded = false;
 
-        public Vector2[] targets;
-        public bool activated = false;
-
-        public float fall_threshold = 200f;
-
-
-        public VergeBlock(EntityData data, Vector2 offset, EntityID id) : base(data, offset)
-        {
-
-            fall_threshold = data.Float("fall_threshold");
-
-            targets = new Vector2[data.Nodes.Length];
-            for(int i = 0; i < targets.Length; ++i)
-            {
-                targets[i] = data.Nodes[i]+offset;
-            }
-
-            base.node = null;
-
-        }
-
         public static void Load()
         {
             if(loaded) return;
@@ -81,6 +60,120 @@ namespace Celeste.Mod.ErrandOfWednesday
             orig(player, data);
         }
 
+
+        public float drift = 0.3f;
+
+        public Vector2[] targets;
+        public bool activated = false;
+
+        public float fall_threshold = 200f;
+
+        public float[] border_alpha;
+        public int[] border_sequence;
+
+        public VergeBlock(EntityData data, Vector2 offset, EntityID id) : base(data, offset)
+        {
+
+            border_alpha = new float[7];
+            border_sequence = new int[7];
+            for(int i = 0; i < border_alpha.Length; ++i)
+            {
+                border_alpha[i] = 0;
+                border_sequence[i] = 0;
+            }
+            fall_threshold = data.Float("fall_threshold");
+
+            targets = new Vector2[data.Nodes.Length];
+            for(int i = 0; i < targets.Length; ++i)
+            {
+                targets[i] = data.Nodes[i]+offset;
+            }
+
+//            base.node = null;
+
+        }
+
+        public float get_wobble(int x_index, int y_index)
+        {
+            int seq_idx = Math.Abs(x_index+y_index)%border_sequence.Length;
+            int sequence = border_sequence[seq_idx];
+            float alpha = border_alpha[seq_idx];
+            float a = (float)Math.Abs((x_index*13+y_index*27+sequence*7)%10);
+            float b = (float)Math.Abs((x_index*13+y_index*27+(sequence+1)*7)%10);
+
+            return drift * (a*(1-alpha) + b*alpha);
+        }
+
+        public override void Render()
+        {
+            Camera camera = SceneAs<Level>().Camera;
+            if (base.Right < camera.Left || base.Left > camera.Right || base.Bottom < camera.Top || base.Top > camera.Bottom)
+            {
+                return;
+            }
+
+            Vector2 block_offset = base.Position + shake;
+
+//Draw.Rect(block_offset.X, block_offset.Y, base.Width, base.Height, Color.White);
+
+
+            int tile_x = (int)Math.Floor(block_offset.X/8);
+            int tile_y = (int)Math.Floor(block_offset.Y/8);
+
+            int right_tile_x = (int)Math.Floor((block_offset.X+base.Width)/8)+1;
+            int bot_tile_y = (int)Math.Floor((block_offset.Y+base.Height)/8)+1;
+
+            Color bg_color = playerHasDreamDash ? activeBackColor : disabledBackColor;
+
+            //vertical slates
+            float w;
+            //Left edge
+            w = 8f*(tile_x+1)-block_offset.X;
+            Draw.Rect(block_offset.X, block_offset.Y, w, base.Height, bg_color);
+
+            //Middle
+            for(int x_index = tile_x+1; x_index < right_tile_x-1;++ x_index)
+            {
+                float wobble_top = 0;
+                float wobble_bot = 0;
+                wobble_top = get_wobble(x_index, tile_y);
+                wobble_bot = get_wobble(x_index, bot_tile_y);
+                Draw.Rect(x_index*8, block_offset.Y-wobble_top, 8, base.Height+wobble_top+wobble_bot, bg_color);
+            }
+            //Right edge
+            w = block_offset.X+base.Width - (right_tile_x-1)*8f;
+            Draw.Rect((right_tile_x-1)*8, block_offset.Y, w, base.Height, bg_color);
+
+
+            //sideways slats
+        
+            for(int y_index = tile_y+1; y_index < bot_tile_y-1;++ y_index)
+            {
+                float wobble_left = 0;
+                float wobble_right = 0;
+                wobble_left = get_wobble(y_index, tile_x);
+                wobble_right = get_wobble(y_index, right_tile_x);
+                Draw.Rect(block_offset.X-wobble_left, 
+                          y_index*8, 
+                          base.Width+wobble_left+wobble_right, 8, bg_color);
+            }
+ 
+
+//            Draw.Rect(block_offset.X, block_offset.Y, base.Width, base.Height, playerHasDreamDash ? activeBackColor : disabledBackColor);
+            Vector2 camera_position = SceneAs<Level>().Camera.Position;            
+ 
+            for(int layer = 0; layer < 1; ++layer)
+            {
+                Vector2 offset = camera_position * (0.3f+0.25f*layer);
+
+                for(float dx = 0; dx < base.Width+8; dx+= 8)
+                {
+                }
+                
+            }
+
+        }
+
         public void activate(Player player)
         {
             if(activated) return;
@@ -103,6 +196,17 @@ namespace Celeste.Mod.ErrandOfWednesday
         public override void Update()
         {
             base.Update();
+            for(int i = 0; i < border_alpha.Length; ++i)
+            {
+                border_alpha[i] += 0.02f;
+                if(border_alpha[i] >= 1)
+                {
+                    border_alpha[i] -= 1;
+                    border_sequence[i] += 1;
+                }
+            }
+ 
+
         }
     }
 }
