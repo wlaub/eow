187feb419fb2a5d7437a4539cfbd343c8ffd8455
wlaub <goldcray@gmail.com>
2026-03-08 18:05:00 -0600

    better jump enable logic

--- entities/jump_off.cs
+++ entities/jump_off.cs
@@ -14,25 +14,20 @@ namespace Celeste.Mod.ErrandOfWednesday
     public class JumpOff : Entity
     {
 
-        public bool activated = false;
 
         public bool player_moving_up = false;
 
         public EntityID eid;
-
-        public Hitbox dash_box;
-        public Hitbox jump_box;
         
         public Vector2 left;
         public Vector2 right;
 
         public bool player_was_above = false;
+        public bool active = false;
 
         public JumpOff(EntityData data, Vector2 offset, EntityID eid) : base(data.Position+ offset)
         {
-            dash_box = new Hitbox(data.Width, 5f, 0, -1);
-            jump_box = new Hitbox(data.Width, 3f);
-            Collider = jump_box;
+            Collider = new Hitbox(data.Width, 5f, 0, -1);
 
             left = new Vector2(Position.X, Position.Y);
             right = new Vector2(Position.X+data.Width, Position.Y);
@@ -58,43 +53,41 @@ namespace Celeste.Mod.ErrandOfWednesday
             {
                 
                 bool player_above = player.Bottom <= Position.Y;
-                if(player_above != player_was_above && player.Right >= Position.X && player.Left <= Position.X+Width) 
+                if(!player_above && player_was_above && player.Right >= Position.X && player.Left <= Position.X+Width) 
                 {
-                    player.jumpGraceTimer=0.1f;
+                    //after falling through, enable jump while in hitbox
+                    player.jumpGraceTimer=0.02f;
+                    active = true;
+                }
+                if(player_above && !player_was_above && player.Right >= Position.X && player.Left <= Position.X+Width) 
+                {
+                    //after jumping through, enable buffered jump
+                    player.jumpGraceTimer=0.02f;
+
                 }
 
-                player_was_above = player_above;
-
-                Collider = dash_box;
-
-                if((player.StateMachine.State == Player.StDash || player.StateMachine.State == Player.StRedDash) && player.Speed.Y ==0 && CollideCheck(player) && player.Bottom <= Bottom)
-                {
-                    player.Position.Y = Position.Y;
-                    player.jumpGraceTimer = 0.1f;
-                }
-/*  
-                Collider = jump_box;
-              
                 if(CollideCheck(player) && player.Bottom <= Bottom)
                 {
-                    if(player.Speed.Y >= 0)
+                    if((player.StateMachine.State == Player.StDash || player.StateMachine.State == Player.StRedDash) && player.Speed.Y ==0)
                     {
+                        //if dashing horizontally inside, give full coyote time and bump to dop
+                        player.Position.Y = Position.Y;
                         player.jumpGraceTimer = 0.1f;
-                        player_moving_up = false;
                     }
-                    else
+                    else if(active)
                     {
-                        player_moving_up = true;
+                        //if the player fell through, allow jump as long as player remains inside
+                        player.jumpGraceTimer = 0.02f;
                     }
+
                 }
-                else if(player_moving_up)
+                else
                 {
-                        player.jumpGraceTimer = 0.05f;
-                        player_moving_up = false;
-                    
+                    active = false;
                 }
-                Collider = dash_box;
-*/
+
+
+                player_was_above = player_above;
             }
         }
 
