4cc6e972335203fe5c3aa9b8de00c33911250b00
wlaub <goldcray@gmail.com>
2026-06-19 15:16:10 -0600

    switch to an xml definition

--- Loenn/entities/multipart_decal.lua
+++ Loenn/entities/multipart_decal.lua
@@ -9,7 +9,8 @@ lookout_decal.placements = {
     name = "multipart_decal",
     data = {
         flag = "",
-        sprite_prefix = "1-forsakencity/flag",
+        sprite = "1-forsakencity/flag",
+        xmlfile = "",
         depth = 8999,
         scaleX = 1.0,
         scaleY = 1.0,
@@ -27,48 +28,48 @@ lookout_decal.fieldOrder = {
 "flag",
 }
 
-function lookout_decal.depth(room, entity)
-    return tonumber(entity.depth or -1000)
-end
-
-
 local file_locations = require("file_locations")
 local mod_handler = require("mods")
 local utils = require("utils")
 local logging = require("logging")
 
+local xmlHandler = require("lib.xml2lua.xmlhandler.tree")
+local xml2lua = require("lib.xml2lua.xml2lua")
+
 function lookout_decal.sprite(room, entity)
-    local sprite_prefix = entity.sprite_prefix
-    local sprite_base, prefix = sprite_prefix:match("^(.-)/?([^/]*)$")
+    local xmlfile = mod_handler.commonModContent .. "/Graphics/" .. entity.xmlfile 
 
-    local basedir = mod_handler.commonModContent .. "/Graphics/Atlases/Gameplay/decals/"..sprite_base
+    local handler = xmlHandler:new()
+    local parser = xml2lua.parser(handler)
+    local xmlString = utils.readAll(xmlfile, "rb")
 
-    logging.info('doing one !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11')
-    logging.info(basedir)
-
-
-    local filenames = {}
-    utils.getFilenames(basedir, true, filenames, function(filename)
-        logging.info(filename)
-        return utils.fileExtension(filename) == "png" and filename:find(prefix, 1, true) == 1
-        end)
-    local sprites = {}
-    for i, filename in ipairs(filenames) do
-        basebase = 'decals/'..sprite_base.."/"
-        local _, sprite = filename:match("^(.-)/?([^/]*)$")
-        sprite = sprite:sub(1,-5)
-        logging.info(filename)
-        if sprite ~= '' then
-            local ds = drawableSprite.fromTexture(basebase..sprite .. "00")
-            if ds == nil then
-                logging.info(basebase .. sprite)
-                ds = drawableSprite.fromTexture(basebase .. sprite)
-            end
-            ds:addPosition(entity.x, entity.y)
-            table.insert(sprites, ds)
-        end
+    if not xmlString then
+        logging.error("can't read it")
     end
-    return sprites
+
+    local xml = utils.stripByteOrderMark(xmlString)
+    parser:parse(xml)
+
+    local sprites = {}
+
+    element = handler.root.Stuff[entity.sprite]
+        base_path = element._attr.path
+        for _, part in pairs(element.part) do
+            texture = base_path .. part._attr.path
+            local ds = drawableSprite.fromTexture(texture, entity)
+            if ds == nil then
+                ds = drawableSprite.fromTexture(texture .. '00', entity)
+            end
+            if ds ~= nil then
+                xoff = part._attr.x or 0
+                yoff = part._attr.y or 0
+                ds:addPosition(xoff, yoff)
+                ds.depth = entity.depth + (part._attr.depth or 0)
+                table.insert(sprites, ds)
+            end
+        end
+
+    return sprites 
 end
 
 function lookout_decal.scale(room, entity)
